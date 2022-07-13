856205afdf4f03d0f1344055c95cd08cf9a91393
JohnC-92 <chongxz@solab.me.ntu.edu.tw>
2022-07-13 14:25:40 +0800

    added my key into dockerfile, app.py and buildspec.yml

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:3.6
+
+ARG MY_KEY
+ENV MY_KEY=$MY_KEY
+
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
+
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello World 999!"
+    key = os.environ.get("MY_KEY")
+    return f"Hello World {key}!"
 
 
 if __name__ == "__main__":
